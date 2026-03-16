64d402e3581b896f45f0ce565b6d519e7d3cefa0
Jobin Kurian <jobink.dev@gmail.com>
2026-03-16 17:50:45 +0530

    v3.6.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .target(name: "SmartechAppInboxSPM", dependencies: []),
         .binaryTarget(name: "SmartechAppInbox",
-                      url: "https://github.com/NetcoreSolutions/SmartechAppInboxSPM/releases/download/3.6.0/SmartechAppInbox.xcframework.zip",
-                     checksum: "dc3d9079dc0b9a0c44949fd0123f3fe6233dce7ef813ceb418f8a4d48a6f8033")
+                      url: "https://github.com/NetcoreSolutions/SmartechAppInboxSPM/releases/download/3.6.1/SmartechAppInbox.xcframework.zip",
+                     checksum: "92f500fd6003c9753725c358ad6f634c499479c02123fe3777f2665086935237")
     ]
 )
